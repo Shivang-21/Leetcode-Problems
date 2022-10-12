de43ae6c62cc260dc3351ef8e0e1d6ff38d70867
Shivang Srivastava <65039159+Shivang-21@users.noreply.github.com>
2022-10-12 16:03:50 +0530

    Time: 9 ms (64.41%), Space: 10.6 MB (25.42%) - LeetHub

---
+++ 0789-escape-the-ghosts/0789-escape-the-ghosts.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool escapeGhosts(vector<vector<int>>& ghosts, vector<int>& target) {
+        int pacman_distance=abs(target[0])+abs(target[1]),n=ghosts.size();
+        vector<int> ghosts_distance(n);
+        for(int i=0;i<n;i++) {
+            if(pacman_distance>=abs(target[0]-ghosts[i][0])+abs(target[1]-ghosts[i][1])) {
+                return false;
+            }
+        }
+        return true;
+    }
+};
