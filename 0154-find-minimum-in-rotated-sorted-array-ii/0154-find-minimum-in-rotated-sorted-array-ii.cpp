830cf1f658d32bec217e53a6f51f54adf5072c1b
Shivang Srivastava <65039159+Shivang-21@users.noreply.github.com>
2022-11-04 09:33:33 +0530

    Time: 12 ms (54.52%), Space: 12.2 MB (69.33%) - LeetHub

--- 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
+++ 0154-find-minimum-in-rotated-sorted-array-ii/0154-find-minimum-in-rotated-sorted-array-ii.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         return bruteforce(nums);
+        return binarysearch(nums);
     }
     int bruteforce(vector<int>& nums) {
         int n=nums.size(),Min=INT_MAX;
@@ -9,5 +10,21 @@ public:
             Min=min(Min,nums[i]);
         }
         return Min;
+    }
+    int binarysearch(vector<int>& a) {
+        int l=0,u=a.size()-1,mid,n=a.size();
+        while(l<=u) {
+            mid=l+(u-l)/2;
+            if(a[mid]>=a[u] and a[mid]<=a[l]) {
+                l=mid+1;
+            }
+            else if(a[mid]<a[u] and a[mid]>a[l]){
+                u=mid-1;
+            }
+            else if(a[mid]<a[l] and a[mid]<a[u]){
+                return a[mid];
+            }
+        }
+        return -1;
     }
 };
